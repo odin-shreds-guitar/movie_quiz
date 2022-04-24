4db2042071d497adb28a63d0f9d20ee7cbd55087
joshua Tkachuk <joshuatkachuk530@gmail.com>
2022-04-23 18:05:55 -0700

    quiz-functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Main from './views/Main';
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Reg from './components/Reg';
 import Login from './components/Login';
+import Quiz from './components/Quiz';
+import Results from './components/Results';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         < Route  default path="/" element={<Main/>}/>
         <Route path="/reg" element={<Reg/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/quiz" element={<Quiz/>}/>
+        <Route path="/results/:correctNum/:totalNum" element={<Results/>}/>
       </Routes>
     </BrowserRouter>
   );
