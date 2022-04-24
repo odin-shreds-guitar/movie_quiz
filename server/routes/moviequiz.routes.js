f13f497cd142c18c13a316f688a3a42cb4e43386
fabriziospurio <fabrizio.spurio@meltwater.com>
2022-04-24 19:42:19 -0400

    cruft-cleanup

--- server/routes/moviequiz.routes.js
+++ server/routes/moviequiz.routes.js
@@ -2,9 +2,4 @@ const MovieQuizController = require('../controllers/moviequiz.controller')
 
 module.exports = function(app){
 	app.get('/', MovieQuizController.index);
-	// app.post('/project', MovieQuizController.createProject);
-	// app.get('/projects', MovieQuizController.getAllProjects);
-	// app.get('/projects/:id', MovieQuizController.getProject);
-	// app.put('/projects/:id', MovieQuizController.updateProject);
-	// app.delete('/projects/:id', MovieQuizController.deleteProject )
 }
