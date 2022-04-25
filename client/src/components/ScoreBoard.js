cc2579c6001e58082fde3357d122cfe924806b75
Mr-Sean <86028295+Mr-Sean@users.noreply.github.com>
2022-04-25 06:05:51 -0700

    In-progress ScoreBoard.js added

---
+++ client/src/components/ScoreBoard.js
@@ -0,0 +1,85 @@
+import React, {useState, useEffect} from "react";
+import {Link, navigate} from "@reach/router"; // "npm i @reach/router --legacy-peer-deps"
+// import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
+const ScoreBoard = () => {
+
+
+    const [user, setUser] = useState(null);
+    const [score, setScore] = useState([]);
+    
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/scores')
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);                
+                setScore(res.data);
+            })
+            .catch((err) => {
+                console.log(err);                
+            })
+    }, [])
+
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/users/logged",
+            {withCredentials: true}
+        )
+            .then((res) => {
+                console.log(res.data);
+                setUser(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+                navigate("/");
+            })
+    },[]);
+
+
+    const deleteScore = (idFromBelow) => {
+        axios
+        .delete(`http://localhost:8000/api/scores/${idFromBelow}`)
+        .then((res) => {
+            console.log(res.data);
+            navigate("/main");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
+
+
+
+    return (
+        <div>
+            <table className="table table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Rank</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Score</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+
+        </div>
+
+    )
+
+
+}
+
+export default ScoreBoard;
