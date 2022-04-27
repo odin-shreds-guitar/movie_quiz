eed50a3d436d0eadf85a82b6767ca31b9f78dedb
Mr-Sean <86028295+Mr-Sean@users.noreply.github.com>
2022-04-27 13:48:23 -0700

    Scoreboard Frontend Mostly Done

--- client/src/components/ScoreBoard.js
+++ client/src/components/ScoreBoard.js
@@ -1,89 +1,112 @@
-// import React, {useState, useEffect} from "react";
-// // import {Link, navigate, useNavigate} from "@reach/router"; // "npm i @reach/router --legacy-peer-deps"
-// import { Link, useNavigate } from "react-router-dom";
-// import axios from "axios";
+import React, {useState, useEffect} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
-// const ScoreBoard = () => {
+const ScoreBoard = () => {
 
-//     const navigate = useNavigate()
+    const navigate = useNavigate()
 
-//     const [user, setUser] = useState(null);
-//     const [score, setScore] = useState([]);
+    const [quizScore, setQuizScore] = useState([]);
+
+    const [user, setUser] = useState(null);
     
 
-//     useEffect(() => {
-//         axios.get('http://localhost:8000/api/scores')
-//             .then((res) => {
-//                 console.log(res);
-//                 console.log(res.data);                
-//                 setScore(res.data);
-//             })
-//             .catch((err) => {
-//                 console.log(err);                
-//             })
-//     }, [])
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/scores')
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);                
+                setQuizScore(res.data);
+            })
+            .catch((err) => {
+                console.log(err);                
+            })
+    }, [])
 
 
-//     useEffect(() => {
-//         axios.get("http://localhost:8000/api/users/logged",
-//             {withCredentials: true}
-//         )
-//             .then((res) => {
-//                 console.log(res.data);
-//                 setUser(res.data);
-//             })
-//             .catch((err) => {
-//                 console.log(err);
-//                 navigate("/");
-//             })
-//     },[]);
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/users/logged",
+            {withCredentials: true}
+        )
+            .then((res) => {
+                console.log(res.data);
+                setUser(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+                navigate("/");
+            })
+    },[]);
 
 
-//     const deleteScore = (idFromBelow) => {
-//         axios
-//         .delete(`http://localhost:8000/api/scores/${idFromBelow}`)
-//         .then((res) => {
-//             console.log(res.data);
-//             navigate("/main");
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-//     };
+    const deleteScore = (idFromBelow) => {
+        axios
+        .delete(`http://localhost:8000/api/scores/${idFromBelow}`)
+        .then((res) => {
+            console.log(res.data);
+            navigate("/main");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
 
 
 
-//     return (
-//         <div>
-//             <table className="table table-dark">
-//                 <thead>
-//                     <tr>
-//                         <th scope="col">Rank</th>
-//                         <th scope="col">Name</th>
-//                         <th scope="col">Score</th>
-//                         <th scope="col">Actions</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     {user.map((score, index) => (
-                        
-//                         <tr>
-//                         <td scope="row">1</td>
+    return (
+        <div>
+            <table className="table table-dark">
 
-//                         <td></td>
-//                         <td></td>
-//                         <td></td>
-//                     </tr>
-//                     ))}
-//                 </tbody>
-//             </table>
+                <thead>
+                    <tr>
+                        <th scope="col">Rank</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Score</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
 
-//         </div>
+                <tbody>
+                    {   
+                        quizScore?
 
-//     )
+                        quizScore.map((score, index) => (
+                            
+                            <tr key={index}>
+
+                                <td scope="row"> // no time to make ranking algo
+                                    n/a                                
+                                </td>
+
+                                <td>
+                                <Link to={`/user/userprofile/${score.createdBy.username}`}>
+                                        {score.createdBy.username}
+                                </Link>
+                                </td>
+
+                                <td>
+                                    {score.score}
+                                </td>
+
+                                <td>
+                                    <button 
+                                        onClick={(e) => deleteScore(score._id)}
+                                        style={{backgroundColor:"red", color:"white"}}
+                                        >DELETE</button>
+                                </td>
+                        </tr>
+                        ))
+                        :null
+                    }
+                </tbody>
+            </table>
+
+        </div>
+
+    )
 
 
-// }
+}
 
-// export default ScoreBoard;
+export default ScoreBoard;
